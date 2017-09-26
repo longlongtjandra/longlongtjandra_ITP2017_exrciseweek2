9dffcb2084f4019aa74811d6d0760dd4dd5d8ee8
longlongtjandra <longlongtjandra2150@gmail.com>
2017-09-26 15:05:46 +0700

    exerciseweek2

---
+++ battery.py
@@ -0,0 +1,49 @@
+battery=2
+print(battery)
+print("I am a robot")
+
+
+while(battery>0):
+    #if(battery ==0):
+        #print("bye")
+        #break
+    # print("NEW: ",battery)
+    action=input("please input action lol:")
+    if (action=="nothing"):
+        shutdown=input("do you want to shut down?")
+        if(shutdown== "yes"):
+            break
+
+        elif(shutdown=="no"):
+            action=input("please input an action")
+
+    elif(action.isalpha != False):
+        new_battery=(int(battery))-1
+        print(new_battery)
+        print(action)
+
+
+        charge=input("do you want to charge?")
+        if(charge=="yes"):
+            battery=(int(new_battery)+1)
+            # print(battery)
+            action=input("please input action zzz:")
+
+        else:
+            battery=new_battery
+            # print(battery)
+
+
+
+
+    # elif(battery == 0):
+    #     print("masuk")
+    #     break
+
+ 
+
+
+
+
+
+
